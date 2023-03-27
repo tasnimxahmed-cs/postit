2e825f4b9aaa1930e3a5d56e172eb56a74b3baf2
Tasnim Ahmed <tasnimxahmed@gmail.com>
2023-03-27 05:02:26 -0400

    init

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"]
+  },
+}
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export const authOptions =
+{
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+}
+
+export default NextAuth(authOptions)
